ec770364e8a15a85bc0d2a8d503799e2a79221b4
Pyuzya <73394151+Pyuzya@users.noreply.github.com>
2023-06-17 00:06:16 +0500

    123

---
+++ src/components/Tasks.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface TasksProps {}
+
+const Tasks: FunctionComponent<TasksProps> = () => {
+    return (
+        <div className="Task_main">
+            <div className="Task_title"></div>
+        </div>
+    );
+};
+
+export default Tasks;
